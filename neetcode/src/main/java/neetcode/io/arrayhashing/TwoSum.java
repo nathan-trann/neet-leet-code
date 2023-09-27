ded4c61874e46b8fba4f85683097e286c5cae945
nathan <tuananhtran.3103@gmail.com>
2023-09-27 23:07:59 +0700

    Placeholder code for the solution

---
+++ neetcode/src/main/java/neetcode/io/arrayhashing/TwoSum.java
@@ -0,0 +1,15 @@
+package neetcode.io.arrayhashing;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {2,7,11,15};
+        int target = 9;
+
+        System.out.println(twoSum(nums, target));
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        return new int[]{};
+    }
+}
